9c2b66b4a6f9dc34c2127c76255e7f38a67b9e10
Riddhesh More <priyanshumore007@gmail.com>
2022-09-16 23:53:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Example from './components/signup'
+import Header from './components/header'
+
+function App() {
+  return (
+    <div className="App">
+    <Header/>
+    <Example/>
+    </div>
+  );
+}
+
+export default App;
