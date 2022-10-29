61cfb7971adb2b12756a505f0046c9dcf2bfc05d
Riddhesh More <priyanshumore007@gmail.com>
2022-10-30 00:05:53 +0530

    minor

--- src/components/Teacher_Info.js
+++ src/components/Teacher_Info.js
@@ -56,24 +56,24 @@ export default function TeacherInfo(){
                   ))}
                 
                 </div>
-                        <div className="-mr-2 flex items-center md:hidden">
-                          <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                            <span className="sr-only">Open main menu</span>
-                            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-                          </Popover.Button>
-                        </div>
-                <Box className="hidden md:flex" sx={{ p: 1  }}>
+                <Box className="md:flex -mr-4" sx={{ p: 1  }}>
                       <FiberManualRecordIcon
                         fontSize="medium"
                         sx={{
                             mr: 1,
                             color: '#4caf50' ,
                         }}
-                      /><Typography className="text-gra">
+                      /><Typography className="hidden md:flex">
 
                       Present
                       </Typography>
                     </Box>
+                        <div className="-mr-2 flex items-center md:hidden">
+                          <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                            <span className="sr-only">Open main menu</span>
+                            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                          </Popover.Button>
+                        </div>
               </nav>
               <Typography className="pt-12">
                 Special Message:
